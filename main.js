28b7997ec60ca358b26d82f422f425a7bba7dbb9
Cbazil <chadbosch08@gmail.com>
2020-07-31 17:12:05 +0200

    sents id through with onclick event (vue Method)

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+
 new Vue({
   el: "#app",
   data: {
-    msg: "Hello World",
+    player: "player",
+    sq1: 0,
+    sq2: 0,
+    sq3: 0,
+    sq4: 0,
+    sq5: 0,
+    sq6: 0,
+    sq7: 0,
+    sq8: 0,
+    sq9: 0
+  },
+  methods: {
+    move(event) {
+      let targetId = event.currentTarget.id;
+      console.log(targetId);
+    }
   }
 })
