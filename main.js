10353f3981ad8a682add34d4db06b85fcd6dfa29
Cbazil <chadbosch08@gmail.com>
2020-08-05 22:50:19 +0200

    Game itself complete

--- main.js
+++ main.js
@@ -17,7 +17,11 @@ new Vue({
   },
   updated(){
     /* console.log(JSON.stringify(this.squares)); */
+    if (this.play === 1) {
+      console.log('Game Over!');
+    }
   },
+  
   methods: {
     move(event) {
       let targetId = event.currentTarget.id;
@@ -35,48 +39,59 @@ new Vue({
     sq8 = this.squares[7].val;
     sq9 = this.squares[8].val;
     // console.log(sq5);
+    
+    this.botMove(this.squares);    
         
     if(sq1 == 'tic' && sq2 == 'tic' && sq3 == 'tic'){
       this.play = 1;
-    } else if(sq1 == 'tic' && sq4 == 'tic' && sq7 == 'tic'){
+    } 
+    if(sq1 == 'tic' && sq4 == 'tic' && sq7 == 'tic'){
       this.play = 1;
-    } else if(sq3 == 'tic' && sq6 == 'tic' && sq9 == 'tic'){
+    } 
+    if(sq3 == 'tic' && sq6 == 'tic' && sq9 == 'tic'){
       this.play = 1;
-    } else if(sq7 == 'tic' && sq8 == 'tic' && sq9 == 'tic'){
+    } 
+    if(sq7 == 'tic' && sq8 == 'tic' && sq9 == 'tic'){
       this.play = 1;
-    } else if(sq2 == 'tic' && sq5 == 'tic' && sq8 == 'tic'){
+    } 
+    if(sq2 == 'tic' && sq5 == 'tic' && sq8 == 'tic'){
       this.play = 1;
-    } else if(sq4 == 'tic' && sq5 == 'tic' && sq6 == 'tic'){
+    } 
+    if(sq4 == 'tic' && sq5 == 'tic' && sq6 == 'tic'){
       this.play = 1;
-    } else if(sq1 == 'tic' && sq5 == 'tic' && sq9 == 'tic'){
+    } 
+    if(sq1 == 'tic' && sq5 == 'tic' && sq9 == 'tic'){
       this.play = 1;
-    } else if(sq3 == 'tic' && sq5 == 'tic' && sq7 == 'tic'){
+    } 
+    if(sq3 == 'tic' && sq5 == 'tic' && sq7 == 'tic'){
       this.play = 1;
-    } else if(sq1 == 'tac' && sq2 == 'tac' && sq3 == 'tac') {
+    } 
+    if(sq1 == 'tac' && sq2 == 'tac' && sq3 == 'tac') {
       this.play = 1;
-    } else if (sq1 == 'tac' && sq4 == 'tac' && sq7 == 'tac') {
+    } 
+    if (sq1 == 'tac' && sq4 == 'tac' && sq7 == 'tac') {
       this.play = 1;
-    } else if (sq3 == 'tac' && sq6 == 'tac' && sq9 == 'tac') {
+    } 
+    if (sq3 == 'tac' && sq6 == 'tac' && sq9 == 'tac') {
       this.play = 1;
-    } else if (sq7 == 'tac' && sq8 == 'tac' && sq9 == 'tic') {
+    } 
+    if (sq7 == 'tac' && sq8 == 'tac' && sq9 == 'tic') {
       this.play = 1;
-    } else if (sq2 == 'tac' && sq5 == 'tac' && sq8 == 'tac') {
+    } 
+    if (sq2 == 'tac' && sq5 == 'tac' && sq8 == 'tac') {
       this.play = 1;
-    } else if (sq4 == 'tac' && sq5 == 'tac' && sq6 == 'tac') {
+    } 
+    if (sq4 == 'tac' && sq5 == 'tac' && sq6 == 'tac') {
       this.play = 1;
-    } else if (sq1 == 'tac' && sq5 == 'tac' && sq9 == 'tac') {
+    } 
+    if (sq1 == 'tac' && sq5 == 'tac' && sq9 == 'tac') {
       this.play = 1;
-    } else if (sq3 == 'tac' && sq5 == 'tac' && sq7 == 'tac') {
-      this.play = 1;
-    } else {
-      this.play = 0;
     }
+    if (sq3 == 'tac' && sq5 == 'tac' && sq7 == 'tac') {
+      this.play = 1;
+    } 
     
-    if (this.play === 1){
-        console.log('Game Over!');
-    }
     // this.turn = !this.turn;
-    this.botMove(this.squares);
     },
     randumb(arr1, arr2) {
       var percent = Math.ceil(Math.random() * 100);
@@ -92,7 +107,7 @@ new Vue({
       }
     },
     botMove(sqrs){
-      console.log(JSON.stringify(sqrs));
+      // console.log(JSON.stringify(sqrs));
       let sq1 = sqrs[0].val;
       let sq2 = sqrs[1].val;
       let sq3 = sqrs[2].val;
@@ -104,202 +119,209 @@ new Vue({
       let sq9 = sqrs[8].val;
       
       // Player Plays middle
-      
       if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[0], sqrs[8]], [sqrs[1], sqrs[2], sqrs[3], sqrs[7]]);
         move.val = 'tac';
       } 
       
       // Phase 1 good move
-      if(sq1 == 'tac' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == '') {
+      else if(sq1 == 'tac' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == '') {
         let move = this.randumb([sqrs[6]], [sqrs[3], sqrs[7]]);
         move.val = 'tac';
       }
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 ==  '' && sq5 == 'tic' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == 'tac'){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 ==  '' && sq5 == 'tic' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == 'tac'){
         let move = this.randumb([sqrs[2]], [sqrs[1], sqrs[5]]);
         move.val = 'tac';
       }
-      if(sq1 == '' && sq2 == '' && sq3 == 'tac' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == 'tic'){
+      else if(sq1 == '' && sq2 == '' && sq3 == 'tac' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == 'tic'){
         let move = this.randumb([sqrs[0]], [sqrs[1], sqrs[3]]);
         move.val = 'tac';
       }
       // Phase 1 Bad move
-      if(sq1 == '' && sq2 == 'tac' && sq3 == 'tic' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == '' && sq2 == 'tac' && sq3 == 'tic' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[6]], [sqrs[6], sqrs[3], sqrs[7]]);
         move.val = 'tac';
       } 
  
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == 'tic' && sq8 == 'tac' && sq9 == ''){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tic' && sq6 == '' && sq7 == 'tic' && sq8 == 'tac' && sq9 == ''){
         let move = this.randumb([sqrs[2]], [sqrs[1], sqrs[2], sqrs[5]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq2 == '' && sq3 == '' && sq4== 'tac' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == 'tic' && sq2 == '' && sq3 == '' && sq4== 'tac' && sq5 == 'tic' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[8]], [sqrs[5], sqrs[7], sqrs[8]]);
         move.val = 'tac';
       }
       // Player Plays any corner
       // first corner
-      if(sq1 == 'tic' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == 'tic' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[1], sqrs[2], sqrs[3], sqrs[4], sqrs[6], sqrs[8]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == ''){
+      else if(sq1 == 'tic' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[3]], [sqrs[1], sqrs[3], sqrs[5], sqrs[7]]);
           move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == 'tic' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[1]], [sqrs[1], sqrs[3], sqrs[5], sqrs[7]]);
         move.val = 'tac';
       }
       // second corner
-      if(sq1 == '' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == '' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[0], sqrs[1], sqrs[4], sqrs[5], sqrs[6], sqrs[8]]);
         move.val = 'tac';
       }
-      if(sq1 == '' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == 'tic'){
+      else if(sq1 == '' && sq2 == '' && sq3 == 'tic' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == 'tic'){
         let move = this.randumb([sqrs[5]], [sqrs[1], sqrs[3], sqrs[5], sqrs[7]]);
         move.val = 'tac';
       }
       // third corner 
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == ''){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[0], sqrs[2], sqrs[3], sqrs[4], sqrs[7], sqrs[8]]);
         move.val = 'tac';
       } 
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == 'tic'){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == 'tac' && sq6 == '' && sq7 == 'tic' && sq8 == '' && sq9 == 'tic'){
         let move = this.randumb([sqrs[7]], [sqrs[1], sqrs[3], sqrs[5], sqrs[7]]);
         move.val = 'tac';
       }
       // fourth corner 
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == 'tic'){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == 'tic'){
         let move = this.randumb([sqrs[4]], [sqrs[0], sqrs[2], sqrs[4], sqrs[5], sqrs[6], sqrs[7]]);
         move.val = 'tac';
       }
       // Player plays sides 
       // top side
-      if(sq1 == '' && sq2 == 'tic' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == '' && sq2 == 'tic' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[0], sqrs[2], sqrs[4]]);
         move.val = 'tac';
       }
       // left side
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == 'tic' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == 'tic' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[0], sqrs[4], sqrs[6]]);
         move.val = 'tac';
       } 
       // right side
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == 'tic' && sq7 == '' && sq8 == '' && sq9 == ''){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == 'tic' && sq7 == '' && sq8 == '' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[2], sqrs[4], sqrs[8]]);
         move.val = 'tac';
       }
       // bottom side 
-      if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == 'tic' && sq9 == ''){
+      else if(sq1 == '' && sq2 == '' && sq3 == '' && sq4 == '' && sq5 == '' && sq6 == '' && sq7 == '' && sq8 == 'tic' && sq9 == ''){
         let move = this.randumb([sqrs[4]], [sqrs[4], sqrs[6], sqrs[8]]);
         move.val = 'tac';
       }
       // Winning moves
       // top side
-      if(sq1 == '' && sq2 == 'tic' && sq2 == 'tic'){
+      else if(sq1 == '' && sq2 == 'tic' && sq2 == 'tic' || sq1 == '' && sq2 == 'tac' && sq2 == 'tac'){
         let move = this.randumb([sqrs[0]], [sqrs[0]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq2 == '' && sq3 == 'tic'){
+      else if(sq1 == 'tic' && sq2 == '' && sq3 == 'tic' || sq1 == 'tac' && sq2 == '' && sq3 == 'tac'){
         let move = this.randumb([sqrs[1]], [sqrs[1]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq2 == 'tic' && sq3 == ''){
+      else if(sq1 == 'tic' && sq2 == 'tic' && sq3 == '' || sq1 == 'tac' && sq2 == 'tac' && sq3 == ''){
         let move = this.randumb([sqrs[2]],[sqrs[2]]);
         move.val = 'tac';
       }
       // left side
-      if(sq1 == '' && sq4 == 'tic' && sq7 == 'tic'){
+      else if(sq1 == '' && sq4 == 'tic' && sq7 == 'tic' || sq1 == '' && sq4 == 'tac' && sq7 == 'tac'){
         let move = this.randumb([sqrs[0]], [sqrs[0]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq4 == '' && sq7 == 'tic'){
+      else if(sq1 == 'tic' && sq4 == '' && sq7 == 'tic' || sq1 == 'tac' && sq4 == '' && sq7 == 'tac'){
         let move = this.randumb([sqrs[3]], [sqrs[3]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq4 == 'tic' && sq7 == ''){
+      else if(sq1 == 'tic' && sq4 == 'tic' && sq7 == '' || sq1 == 'tac' && sq4 == 'tac' && sq7 == ''){
         let move = this.randumb([sqrs[6]], [sqrs[6]]);
         move.val = 'tac';
       }
       // right side 
-      if(sq3 == '' && sq6 == 'tic' && sq9 == 'tic'){
+      else if(sq3 == '' && sq6 == 'tic' && sq9 == 'tic' || sq3 == '' && sq6 == 'tac' && sq9 == 'tac'){
         let move = this.randumb([sqrs[2]], [sqrs[2]]);
         move.val = 'tac';
       }
-      if(sq3 == 'tic' && sq6 == '' && sq9 == 'tic'){
+      else if(sq3 == 'tic' && sq6 == '' && sq9 == 'tic' || sq3 == 'tac' && sq6 == '' && sq9 == 'tac'){
         let move = this.randumb([sqrs[5]], [sqrs[5]]);
         move.val = 'tac';
       }
-      if(sq3 == 'tic' && sq6 == 'tic' && sq9 == ''){
+      else if(sq3 == 'tic' && sq6 == 'tic' && sq9 == '' || sq3 == 'tac' && sq6 == 'tac' && sq9 == ''){
         let move = this.randumb([sqrs[8]],[sqrs[8]]);
         move.val = 'tac';
       }
       // bottom side 
-      if(sq7 == '' && sq8 == 'tic' && sq9 == 'tic'){
+      else if(sq7 == '' && sq8 == 'tic' && sq9 == 'tic' || sq7 == '' && sq8 == 'tac' && sq9 == 'tac'){
         let move = this.randumb([sqrs[6]], [sqrs[6]]);
         move.val = 'tac';
       }
-      if(sq7 == 'tic' && sq8 == '' && sq9 == 'tic'){
+      else if(sq7 == 'tic' && sq8 == '' && sq9 == 'tic' || sq7 == 'tac' && sq8 == '' && sq9 == 'tac'){
         let move = this.randumb([sqrs[7]], [sqrs[7]]);
         move.val = 'tac';
       }
-      if(sq7 == 'tic' && sq8 == 'tic' && sq9 == ''){
+      else if(sq7 == 'tic' && sq8 == 'tic' && sq9 == '' || sq7 == 'tac' && sq8 == 'tac' && sq9 == ''){
         let move = this.randumb([sqrs[8]], [sqrs[8]]);
         move.val = 'tac';
       }
       // verticle 
-      if(sq2 == '' && sq5 == 'tic' && sq8 == 'tic'){
+      else if(sq2 == '' && sq5 == 'tic' && sq8 == 'tic' || sq2 == '' && sq5 == 'tac' && sq8 == 'tac'){
         let move = this.randumb([sqrs[1]], [sqrs[1]]);
         move.val = 'tac';
       }
-      if(sq2 == 'tic' && sq5 == '' && sq8 == 'tic'){
+      else if(sq2 == 'tic' && sq5 == '' && sq8 == 'tic' || sq2 == 'tac' && sq5 == '' && sq8 == 'tac'){
         let move = this.randumb([sqrs[4]], [sqrs[4]]);
         move.val = 'tac';
       }
-      if(sq2 == 'tic' && sq5 == 'tic' && sq8 == ''){
+      else if(sq2 == 'tic' && sq5 == 'tic' && sq8 == '' || sq2 == 'tac' && sq5 == 'tac' && sq8 == ''){
         let move = this.randumb([sqrs[7]], [sqrs[7]]);
         move.val = 'tac';
       }
       // horizontal
-      if(sq4 == '' && sq5 == 'tic' && sq6 == 'tic'){
+      else if(sq4 == '' && sq5 == 'tic' && sq6 == 'tic' || sq4 == '' && sq5 == 'tac' && sq6 == 'tac'){
         let move = this.randumb([sqrs[3]], [sqrs[3]]);
         move.val = 'tac';
       }
-      if(sq4 == 'tic' && sq5 == '' && sq6 == 'tic'){
+      else if(sq4 == 'tic' && sq5 == '' && sq6 == 'tic' || sq4 == 'tac' && sq5 == '' && sq6 == 'tac'){
         let move = this.randumb([sqrs[4]], [sqrs[4]]);
         move.val = 'tac';
       }
-      if(sq4 == 'tic' && sq5 == 'tic' && sq6 == ''){
+      else if(sq4 == 'tic' && sq5 == 'tic' && sq6 == '' || sq4 == 'tac' && sq5 == 'tac' && sq6 == ''){
         let move = this.randumb([sqrs[5]], [sqrs[5]]);
         move.val = 'tac';
       }
       // Diagonal right
-      if(sq1 == '' && sq5 == 'tic' && sq9 == 'tic'){
+      else if(sq1 == '' && sq5 == 'tic' && sq9 == 'tic' || sq1 == '' && sq5 == 'tac' && sq9 == 'tac'){
         let move = this.randumb([sqrs[0]], [sqrs[0]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq5 == '' && sq9 == 'tic'){
+      else if(sq1 == 'tic' && sq5 == '' && sq9 == 'tic' || sq1 == 'tac' && sq5 == '' && sq9 == 'tac'){
         let move = this.randumb([sqrs[4]], [sqrs[4]]);
         move.val = 'tac';
       }
-      if(sq1 == 'tic' && sq5 == 'tic' && sq9 == ''){
+      else if(sq1 == 'tic' && sq5 == 'tic' && sq9 == '' || sq1 == 'tac' && sq5 == 'tac' && sq9 == ''){
         let move = this.randumb([sqrs[8]], [sqrs[8]]);
         move.val = 'tac';
       }
       // Diagonal left
-      if(sq3 == '' && sq5 == 'tic' && sq7 == 'tic'){
+      else if(sq3 == '' && sq5 == 'tic' && sq7 == 'tic' || sq3 == '' && sq5 == 'tac' && sq7 == 'tac'){
         let move = this.randumb([sqrs[2]], [sqrs[2]]);
         move.val = 'tac';
       }
-      if(sq3 == 'tic' && sq5 == '' && sq7 == 'tic'){
+      else if(sq3 == 'tic' && sq5 == '' && sq7 == 'tic' || sq3 == 'tac' && sq5 == '' && sq7 == 'tac'){
         let move = this.randumb([sqrs[4]], [sqrs[4]]);
         move.val = 'tac';
       }
-      if(sq3 == 'tic' && sq5 == 'tic' && sq7 == ''){
+      else if(sq3 == 'tic' && sq5 == 'tic' && sq7 == '' || sq3 == 'tac' && sq5 == 'tac' && sq7 == ''){
         let move = this.randumb([sqrs[6]], [sqrs[6]]);
         move.val = 'tac';
       }
-    }  
+      else {
+        this.randomSqr(sqrs);
+      }
+    },
+    randomSqr(arr) {
+      let empty = arr.filter(item => item.val == "");
+      let move = empty[Math.floor(Math.random() * empty.length)];
+      move.val = 'tac';
+    }
   } 
 })
